c24e9912828fd0b9b5e9e00d02685ceaf569598e
halls510 <halls510@hotmail.com>
2021-12-07 10:53:36 -0300

    [INSERT] - Foi inserido o Formulário Valida Senha.

--- CursoWindowsForms/Program.cs
+++ CursoWindowsForms/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CursoWindowsForms
@@ -16,7 +13,7 @@ namespace CursoWindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_DemonstracaoKey());
+            Application.Run(new Frm_ValidaSenha());
         }
     }
 }
